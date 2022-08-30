bbaf4cfaed0e8e81463a9e6466752f123b5f9c1f
DmytroRoshka <roschka@ukr.net>
2022-08-30 21:01:20 +0000

    Add

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,20 +1,8 @@
-(function (){
-    const iconBurger = document.querySelector('.navigation-burger');
-    const navigationsBody = document.querySelector('.navigation-wrapper');
-    iconBurger.addEventListener('click', function (e) {
-        document.body.classList.toggle('lock');
-        iconBurger.classList.toggle('active');
-        navigationsBody.classList.toggle('active');
-    });
-const navLinks = document.querySelectorAll('.navigation__link');
-navLinks.forEach((navLink) => {
-    navLink.addEventListener('click', (_) => {
-        if (iconBurger.classList.contains('active')) {
-            document.body.classList.remove('lock');
-            iconBurger.classList.remove('active');
-            navigationsBody.classList.remove('active');
-        }
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
     }
-    );
-});
-})();
+  }
